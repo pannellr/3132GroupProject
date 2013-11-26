cf72e80a46f487556cd53ee82c4e725ff93114e1
Richard Pannell <richard.pannell@gmail.com>
2013-11-25 20:16:45 -0400

    adding post submit form

--- controllers/mastercontroller.py
+++ controllers/mastercontroller.py
@@ -3,10 +3,8 @@
 class MasterController:
 
     def markup(self, posts):
-        #path = ('views/', args['class'], '/', args['method'], '.html')
-        #markup = open(path, "r").read()
+        
         markup = ''
-
         markup += '<div id="wrap2">'
         markup += '<img border="0" src="gglogo.png" alt="gogeo logo" width="250" height="125">'
         markup += '<img border="0" src="banner.png" alt="banner" width="1000" height="125">'
@@ -36,6 +34,16 @@ class MasterController:
             markup += '</div>'
             markup += '<span class="post-date"><em>' + post[0]['created_at'].strftime('%m/%d/%Y') + '</em></span>'
             markup += '</div>'
+            # add options here
             markup += '<hr />'
-                        
+
+
+        # test session state here
+        markup += '<form action="post/create" method="POST" class="post-form">'
+        markup += '<p>'
+        markup += '<label for="post">Post</label><br />'
+        markup += '<textarea name="post" rows="5" cols="80"></textarea><br />'
+        markup += '<input type="submit" value="Post" class="post-form-submit"/>'
+        markup += '</p>'
+        
         return markup
